7370163323a486ee28541342dba05c619cba678c
Nako <nako2875@gmail.com>
2022-05-06 16:34:23 +0300

    poezd kupe

--- script.js
+++ script.js
@@ -1,71 +1,18 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+function getCoupeNumber(setnumber) {
+	if (typeof (setnumber) !== 'number' || setnumber < 0 || !Number.isInteger(setnumber)) {
+		return 'Ошибка.Проверьте правильность веденного номера места!';
 	}
-}
+	if (setnumber == 0 || setnumber > 36) {
+		return 'Таких мест в вагоне не существует!';
+	}
 
-start();
-
-
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-
-function rememberMyFilms() {
-	for (let i = 0; i < 2; i++) {
-		const a = prompt('Один из последних посмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', '');
-		if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('done');
-		} else {
-			console.log('error');
-			i--;
+	for (let i = 4; i < 36; i += 4) {
+		if (setnumber < i) {
+			return Math.ceil(i / 4);
 		}
 	}
 }
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log('Посмотрено довольно мало фильмов!');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-		console.log('Вы классический зритель');
-	} else if (personalMovieDB.count >= 30) {
-		console.log('Вы киноман!');
-	} else {
-		console.log('Произошла ошибка');
-	}
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-	if (!hidden) {
-		console.log(personalMovieDB);
-	}
-}
-
-showMyDB(personalMovieDB.privat);
-
-
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-	}
-}
-
-writeYourGenres();
-
+console.log(getCoupeNumber(10));
